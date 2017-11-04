f6f05b3c63ad070d3380eedcb4bfbc6f373560c6
Tymoteusz Machowski <tmachows@gmail.com>
2017-11-04 16:32:21 +0100

    make client interactive + rewrite to boto3

--- client.py
+++ client.py
@@ -1,13 +1,31 @@
-from sqs_launcher import SqsLauncher
-import time
+import boto3
 
 
-launcher = SqsLauncher('iosrFastPaxos_node1')
+nodes_queues = ['iosrFastPaxos_node1']
+response_queue_name = 'iosrFastPaxos_client1'
+sqs = boto3.resource('sqs')
 
-launcher.launch_message({'command': 'set', 'key': 'message', 'value': 'Hello World!'})
 
-print(" [x] Sent 'Hello World!'")
+def get_value(key):
+    pass
 
-time.sleep(5)
+def set_value(key, value):
+    for queue_name in nodes_queues:
+        queue = sqs.get_queue_by_name(QueueName=queue_name)
+        queue.send_message(MessageBody={'command': 'accept', 'key': key, 'value': value, 
+                                'response_queue': response_queue_name})
+                            
+    response_queue = sqs.get_queue_by_name(QueueName=response_queue_name)
+    for message in queue.receive_messages(WaitTimeSeconds=5):
+        print("Response: ", message.body)
 
-launcher.launch_message({'command': 'print'})
+
+while True:
+    action = input("Enter 'g' to get value by key or 's' to save value: ")
+    if action == 'g':
+        key = input("Key: ")
+        print("Value is: ", get_value(key))
+    elif action == 's':
+        key = input("Key: ")
+        value = input("Value: ")
+        set_value(key, value)
